f5685976e299ccbef3f18031b794818a3084cd6e
Louinardo Holzken <93583285+Louinardo@users.noreply.github.com>
2022-09-15 15:21:15 +0200

    Begin

---
+++ backend/config.php
@@ -0,0 +1,13 @@
+<?php
+
+#### TODO
+//Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
+//Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
+
+$dbHost = 'localhost';
+$dbName = 'C1-Louinardo';
+$dbUser = 'root';
+$dbPass = '';
+
+//De url waarop jouw project staat. Géén slash aan het einde.
+$base_url = 'http://localhost/C1-Louinardo';
---
+++ header.php
@@ -0,0 +1,5 @@
+<?php require_once 'backend/config.php'; ?>
+
+<div class="header">
+	<img src="img/logo.png">
+</div>
